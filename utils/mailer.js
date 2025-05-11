0ecd52780c021aa2230a9f2c1a50a636876d3e29
Talhaanwarrr <157596733+Talhaanwarrr@users.noreply.github.com>
2025-05-11 20:56:59 +0500

    fix not defined error

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,21 +1,42 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, text) => {
-
   const transporter = nodemailer.createTransport({
-    host: 'localhost',
-    port: 25,
-    secure: false,             // no TLS inside the same server
-    ignoreTLS: true
-  });
-  
-  await transporter.sendMail({
-    from: 'noreply@tcs.testdevlink.com',   // mailbox created in cPanel
-    to,
-    subject,
-    text
+    host: "stg2.stagingtestserver.com", // matches the cert
+    port: 587, // submission port supports STARTTLS
+    secure: false,
+    auth: {
+      user: "noreply@tcs.testdevlink.com",
+      pass: 'K@+u9zkdPorY', // set this in your .env
+    },
   });
 
+  transporter.sendMail(
+    {
+      from: "noreply@tcs.testdevlink.com",
+      to,
+      subject,
+      text,
+    },
+    (err, info) => {
+      if (err) return console.log("Errorrrr: ", err);
+      console.log("Message sent:", info);
+    }
+  );
+
+  // const transporter = nodemailer.createTransport({
+  //   host: 'localhost',
+  //   port: 25,
+  //   secure: false,             // no TLS inside the same server
+  //   ignoreTLS: true
+  // });
+
+  // await transporter.sendMail({
+  //   from: 'noreply@tcs.testdevlink.com',   // mailbox created in cPanel
+  //   to,
+  //   subject,
+  //   text
+  // });
 
   // const transporter = nodemailer.createTransport({
   //   service: "Gmail", // Or use another SMTP service
