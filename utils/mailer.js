0f92cde79e40b6cd92909fae2e196837bb72a9bb
Talhaanwarrr <157596733+Talhaanwarrr@users.noreply.github.com>
2025-05-11 20:32:05 +0500

    change email config for live

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,20 +1,35 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "Gmail", // Or use another SMTP service
-    auth: {
-      user: process.env.MAIL_USER, // Your email address
-      pass: process.env.MAIL_PASS, // App password
-    },
-  });
 
-  const mailOptions = {
-    from: process.env.MAIL_USER,
+  const transport = nodemailer.createTransport({
+    host: 'localhost',
+    port: 25,
+    secure: false               // no TLS inside the same server
+  });
+  
+  await transport.sendMail({
+    from: 'noreply@tcs.testdevlink.com',   // mailbox created in cPanel
     to,
     subject,
-    text,
-  };
+    text
+  });
+
+
+  // const transporter = nodemailer.createTransport({
+  //   service: "Gmail", // Or use another SMTP service
+  //   auth: {
+  //     user: process.env.MAIL_USER, // Your email address
+  //     pass: process.env.MAIL_PASS, // App password
+  //   },
+  // });
+
+  // const mailOptions = {
+  //   from: process.env.MAIL_USER,
+  //   to,
+  //   subject,
+  //   text,
+  // };
 
   await transporter.sendMail(mailOptions);
 };
